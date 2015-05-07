6d099e464a15a283a88b6bb8313334c8d45f4da9
berlios <berlios@server.fake>
2015-05-07 19:32:18 +0300

    33

---
+++ src/task33.cpp
@@ -0,0 +1,60 @@
+#include <gmpxx.h>
+#include <set>
+#include <utility>
+
+#include "base/task.h"
+
+bool TrivialFraction(int numerator, int denominator) {
+  // Since we consider only fractions with two digits in both numerator and
+  // denominator we can safely throw out all fractions where any of numerator
+  // or denominator ends with a zero.
+
+  return (numerator % 10 == 0 || denominator % 10 == 0);
+}
+
+bool FractionCanBeCancelled(int numerator, int denominator) {
+  if (numerator / 10 == denominator / 10 &&
+      numerator*(denominator % 10) == denominator*(numerator % 10)) {
+    return true;
+  }
+
+  if (numerator % 10 == denominator / 10 &&
+      numerator*(denominator % 10) == denominator*(numerator / 10)) {
+    return true;
+  }
+
+  if (numerator / 10 == denominator % 10 &&
+      numerator*(denominator / 10) == denominator*(numerator % 10)) {
+    return true;
+  }
+
+  if (numerator % 10 == denominator % 10 &&
+      numerator*(denominator / 10) == denominator*(numerator / 10)) {
+    return true;
+  }
+
+  return false;
+
+}
+
+TASK(33) {
+  std::set<std::pair<int, int>> fractions;
+  for (int numerator = 10; numerator < 100; ++numerator) {
+    for (int denominator = 10; denominator < 100; ++denominator) {
+      if (numerator < denominator &&
+          !TrivialFraction(numerator, denominator) &&
+          FractionCanBeCancelled(numerator, denominator)) {
+        fractions.insert(std::make_pair(numerator, denominator));
+      }
+    }
+  }
+
+  mpq_class tmp = 1;
+  for (auto &fraction : fractions) {
+    tmp *= fraction.first;
+    tmp /= fraction.second;
+  }
+
+  tmp.canonicalize();
+  SetResult(tmp.get_den().get_str());
+}
